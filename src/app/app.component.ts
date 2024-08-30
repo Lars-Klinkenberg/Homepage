9a2a9cda2b42f5005581fe8da0ee37a7a1fe02fc
Lars-Klinkenberg <klinkenberg.lars@gmail.com>
2024-08-30 04:08:48 +0200

    added contact page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { PageIndexComponent } from './page-index/page-index.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,7 @@ import { ProjectsComponent } from './projects/projects.component';
     AboutComponent,
     PageIndexComponent,
     ProjectsComponent,
+    ContactComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
@@ -22,9 +24,10 @@ export class AppComponent {
   @ViewChild('home', { read: ElementRef }) homeElement!: ElementRef;
   @ViewChild('about', { read: ElementRef }) aboutElement!: ElementRef;
   @ViewChild('projects', { read: ElementRef }) projectsElement!: ElementRef;
+  @ViewChild('contact', { read: ElementRef }) contactElement!: ElementRef;
 
   title = 'Portfoliopage';
-  allPageIds = ['home', 'about', 'projects'];
+  allPageIds = ['home', 'about', 'projects', 'contact'];
   currentPage = 0;
 
   scrollToElement(elementId: string): void {
@@ -40,6 +43,10 @@ export class AppComponent {
       }
       case 'projects': {
         element = this.projectsElement;
+        break;
+      }
+      case 'contact': {
+        element = this.contactElement;
         break;
       }
       default:
