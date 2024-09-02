83dfa1cee91b53ba7e8c340875ece08d81117dda
Lars-Klinkenberg <klinkenberg.lars@gmail.com>
2024-09-02 21:08:01 +0200

    Update project images and descriptions

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -17,19 +17,21 @@ export class ProjectsComponent {
   allProjects: Project[] = [
     {
       name: 'Chatbots',
-      imagePath: 'assets/placeholder.png',
+      imagePath: 'assets/chatbot.png',
       description:
-        'I worked in a small team on the toom.de and eprimo.de chatbot integration',
+        'Played a pivotal role in the integration and enhancement of chatbot services for customers like toom Baumarkt and eprimo.',
     },
     {
       name: 'Smart Desk',
-      imagePath: 'test',
-      description: 'test',
+      imagePath: 'assets/smart_desk.png',
+      description:
+        'Working on controlling my Desk via Raspeberry Pi to monitor my time standing. Furthermore control the height of the desk.',
     },
     {
       name: 'Resume Page',
-      imagePath: 'test',
-      description: 'My Resume as a Website',
+      imagePath: 'assets/resume.png',
+      description:
+        'A personal resume built as a responsive webpage, designed for seamless viewing on digital devices and optimized for high-quality, professional printing with consistent formatting across platforms.',
     },
   ];
   currentProject = 0;
